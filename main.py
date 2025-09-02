467eb42ade3448db6a10b1dd0fdb972fd772fc41
George Kasparyants <gg_plastt@mail.ru>
2025-09-03 00:00:35 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from pieql import pieql
+
+
+app = FastAPI()
+
+
+@app.get("/items")
+@pieql()
+def items():
+    return JSONResponse({
+        "users": [
+            {"id": 1, "name": "Alice"},
+            {"id": 2, "name": "Bob"},
+            {"id": 3, "name": "Carol"}
+        ]
+    })
